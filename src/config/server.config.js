d11adb30d9efbf6c97c8cb7c1b9795420ac31971
Lex <webdesignbylex@gmail.com>
2019-03-08 15:36:01 -0500

    firebase functions run locally as it should

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -5,9 +5,6 @@ import dotenv from 'dotenv'
 
 // config
 import FEATHERS_APP from './routes.config'
-import {
-  FUNCTIONS_HTTPS_REF, FUNCTIONS_USER_DB_REF
-} from './firebase.config'
 
 /**
  * @file Manages server listening sequence and exposure of Cloud functions
@@ -25,24 +22,11 @@ const SERVER = FEATHERS_APP.listen(PORT)
 SERVER.on('listening', () => {
   let message = `Listening on port ${PORT}`
 
-  if (NODE_ENV === 'development' || NODE_ENV === 'test') {
+  if (NODE_ENV === 'development') {
     message += (`\nServer ready at http://localhost:${PORT}`)
   }
 
   console.info(message)
 })
 
-/**
- * Firebase cloud function that serves the API.
- */
-const api = FUNCTIONS_HTTPS_REF.onRequest(SERVER)
-
-/**
- * Sends a usage report to Slack when a new user is created, and a welcome email
- * to the new user.
- */
-const complete_signup = FUNCTIONS_USER_DB_REF.onCreate((snap, context) => {
-
-})
-
-export { complete_signup, api }
+export default SERVER
--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
+
 // packages
 import dotenv from 'dotenv'
 import '@babel/polyfill'
 
-import './config/server.config'
+// config
+import SERVER from './config/server.config'
+import {
+  FUNCTIONS_HTTPS_REF, FUNCTIONS_USER_DB_REF
+} from './config/firebase.config'
 
 // configure environment variables
 dotenv.config()
+
+/**
+ * Firebase cloud function that serves the API.
+ */
+const api = FUNCTIONS_HTTPS_REF.onRequest((req, res) => {
+  let message = 'Received external request for API.'
+
+  res.send(message)
+  console.info(message)
+})
+
+/**
+ * Sends a usage report to Slack when a new user is created, and a welcome email
+ * to the new user.
+ */
+const complete_signup = FUNCTIONS_USER_DB_REF.onCreate((snap, context) => {
+  console.info('New database user. Triggering complete_signup...\n')
+})
+
+export { api, complete_signup }
